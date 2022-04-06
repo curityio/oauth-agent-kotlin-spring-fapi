d8e42495a7b4a464831ab8b7337c497093e729bb
gary.archer <gary.archer@WORKMAC.local>
2022-04-06 11:53:56 +0200

    Added gradle test logger plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.5.4"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id("com.adarshr.test-logger") version "3.2.0"
 	kotlin("jvm") version "1.5.21"
 	kotlin("plugin.spring") version "1.5.21"
 	id("groovy")
