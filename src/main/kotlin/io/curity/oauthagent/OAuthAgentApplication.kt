5aef9cdfa832f89b1551e7c8d5cf3167a9914d87
gary.archer <gary.archer@WORKMAC.local>
2022-06-17 14:13:01 +0200

    Updates

--- src/main/kotlin/io/curity/oauthagent/OAuthAgentApplication.kt
+++ src/main/kotlin/io/curity/oauthagent/OAuthAgentApplication.kt
@@ -25,7 +25,7 @@ class OAuthAgentApplication {
         config.allowedOrigins = configuration.trustedWebOrigins
         config.allowCredentials = true
         config.allowedMethods = listOf("POST", "GET", "OPTIONS")
-        config.addAllowedHeader("*")
+        config.allowedHeaders = listOf("*")
         source.registerCorsConfiguration("/**", config)
 
         val corsProcessor = CustomCorsProcessor()
